ee04fac2d323408b2167a27d651a1fcdf04aa878
unknown <mrmartin@portal.lab.huskygames.com>
2015-09-18 18:46:36 -0400

    Main menu

---
+++ Astral Titans/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitScript : MonoBehaviour {
+
+    public Sprite mouseExited;
+    public Sprite mouseEntered;
+
+    private bool isClicked;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().sprite = mouseEntered;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().sprite = mouseExited;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Quit was pressed");
+        Application.Quit();
+    }
+}
