b12d83a9b762bea01cc8c20ec420ad62cc0cc630
Joshua <jbhooker@mtu.edu>
2016-02-07 17:18:21 -0500

    Added map prompt and transfer to grid scene

---
+++ Astral Titans/Assets/Scripts/GridScripts/MapGenerationScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+/**
+ * Proceedurally generates maps based on a given size and type
+ */
+public static class MapGeneration {
+	/* Determines if the game map will be created pseudo-randomly */
+	public static bool generate = false;
+	/* the dimensions of the map to create (within the bounds of [5, 45] */
+	public static int width { get; set; }
+	public static int height { get; set; }
+
+	// the type of map to generate (i.e. )
+	private static string map_type;
+
+	static MapGeneration() {
+		width = -1;
+		height = 1;
+	}
+
+	public static List<List<HexScript>> generateMap() {
+		Debug.Log("width: " + width + "\nheight: " + height + "\n");
+		// TODO: actually build the map . . .
+		return null;
+	}
+
+	public static void setGeneration() { generate = true; }
+}
+
---
+++ Astral Titans/Assets/Scripts/MapSpecPrompt/InputPrompt.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+
+public class InputPrompt : MonoBehaviour {
+	private string in_1, in_2, in_3;
+	private string msg_1, msg_2, msg_3;
+	/* [ width min, width max, height min, height max ] */
+	private int[] bounds;
+	/* List of valid map types */
+	private static readonly string[] MAP_TYPES = new string[] { "inland", "highland", "coastal", "ring", "maze" };
+
+	void Start() {
+		in_1 = "";
+		in_2 = "";
+		in_3 = "";
+		msg_1 = "";
+		msg_2 = "";
+		msg_3 = "";
+		bounds = new int[] { 10, 45, 10, 45 };
+	}
+
+	/* Draws the window for the prompt */
+	void OnGUI() {
+		GUI.Window(0, new Rect(400, 200, 600, 400), EvaluateInput, "Map Specs");
+	}
+
+	/**
+	 * Prints information in labels regarding valid input, creates text fields for input,
+	 * and handles any errors with user input.
+	 */
+	void EvaluateInput(int ID) {
+		/* Display general guidelines */
+		GUI.Label(new Rect(45, 45, 510, 88), "Please input the size and type of the map you wish to create: width and height must be within the bounds of 10 and 45 inclusive.\n\nGame types: " + allTypes() );
+		/* Display width and height prompts */
+		GUI.Label(new Rect(45, 165, 124, 22), "Height of the map?");
+		GUI.Label(new Rect(45, 200, 120, 22), "Width of the map?");
+		GUI.Label(new Rect(45, 235, 110, 22), "Type of map?");
+		/* Draw input fields */
+		in_1 = GUI.TextField(new Rect(191, 165, 32, 22), in_1, 3);
+		in_2 = GUI.TextField(new Rect(191, 200, 32, 22), in_2, 3);
+		in_3 = GUI.TextField(new Rect(191, 235, 85, 22), in_3);
+
+		if ( GUI.Button (new Rect (45, 275, 56, 26), "Confirm") ) {
+			int w_in = -1;
+			int h_in = -1;
+
+			/* Parse user input for width value */
+			if ( Int32.TryParse (in_1, out w_in) ) {
+				if ( w_in >= bounds[0] && w_in <= bounds[1] ) {
+					if (w_in % 2 == 0) {
+						MapGeneration.width = w_in;
+						msg_1 = "";
+					} else {
+						msg_1 = "Width must be even!";
+						w_in = -1;
+					}
+				} else {
+					msg_1 = "Not a valid width value!";
+					w_in = -1;
+				}
+			} else {
+				msg_1 = "Input must be an integer!";
+				w_in = -1;
+			}
+
+			/* Parse user input for height value */
+			if ( Int32.TryParse (in_2, out h_in) ) {
+				if (h_in >= bounds[2] && w_in <= bounds[3]) {
+					MapGeneration.height = h_in;
+					msg_2 = "";
+				} else {
+					msg_2 = "Not a valid height value!";
+					h_in = -1;
+				}
+			} else {
+				msg_2 = "Input must be an integer!";
+				h_in = -1;
+			}
+			/* Parse user input for map type */
+			if ( containsType(in_3) ) {
+				/* if all input fields are valid, then jump to creation of the map */
+				if (w_in > 0 && h_in > 0) {
+					SceneManager.LoadScene("grid_scene");
+					// MapGeneration.setGeneration();
+				}
+
+				msg_3 = "";
+			} else {
+				msg_3 = "Not a valid map type!";
+			}
+		}
+
+		/* Print Error messages */
+		GUI.Label(new Rect(235, 165, 146, 22), msg_1);
+		GUI.Label(new Rect(235, 200, 146, 22), msg_2);
+		GUI.Label(new Rect(288, 235, 146, 22), msg_3);
+	}
+
+	/* Determines if the given type is a valid map type */
+	private static bool containsType(string type) {
+		foreach (string t in MAP_TYPES) {
+			if (t == type) { return true; }
+		}
+
+		return false;
+	}
+
+	/* Provides a comma'd list of all map types */
+	private static string allTypes() {
+		string types = "";
+
+		foreach (string t in MAP_TYPES) {
+			types +=  t + ", ";
+		}
+
+		return types.TrimEnd( new char[] { ' ' , ',', ' ' } );
+	}
+}
+
